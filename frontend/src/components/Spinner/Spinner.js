12d997af546a0c08fd07bfb05c2ca594e159c7fd
Abhinav Singh <kittusinghranchi@gmail.com>
2023-09-23 00:50:31 +0530

    added spinner

---
+++ frontend/src/components/Spinner/Spinner.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Spinner.css"
+const Spinner = () => {
+  return (
+
+      <div className="flex justify-center items-center w-[100%] lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+  )
+}
+
+export default Spinner
