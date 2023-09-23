a8e68260d06535d052b6607199ec549c0977535c
addymistrel <addymistrel@gmail.com>
2023-09-23 05:32:09 +0530

    route map added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Homepage from './Pages/Homepage/Homepage';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./Pages/Homepage/Homepage";
+import Maps from "./Pages/Map/RouteMap";
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Homepage/>}/>
-      </Routes>
-    </BrowserRouter>
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/routeDetails" element={<Maps />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
