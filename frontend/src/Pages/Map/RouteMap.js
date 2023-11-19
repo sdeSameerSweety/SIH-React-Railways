c7c814dfb9d62f74922b2e1fff27d63522bebc13
addymistrel <102992848+addymistrel@users.noreply.github.com>
2023-11-20 02:06:40 +0530

    route map enhanced

--- frontend/src/Pages/Map/RouteMap.js
+++ frontend/src/Pages/Map/RouteMap.js
@@ -64,7 +64,47 @@ function MyComponent() {
     nonce: "grabway@123",
   });
 
-  // const [directionResponse, setDirectionResponse] = useState([]);
+  const onway = () => {
+    let tmparr = [];
+    for (var pp = 1; pp < location.state.markers.length - 1; pp++) {
+      tmparr.push({
+        location: {
+          lat: location.state.markers[pp].latitude,
+          lng: location.state.markers[pp].longitude,
+        },
+        stopover: true,
+      });
+    }
+    console.log(tmparr);
+    return tmparr;
+  };
+
+  const [directionResponse, setDirectionResponse] = useState("");
+  async function calculateRoute(org, dest) {
+    /* eslint-disable */
+    const directionService = new google.maps.DirectionsService();
+    const results = await directionService
+      .route({
+        origin: new google.maps.LatLng(org.latitude, org.longitude),
+        destination: new google.maps.LatLng(dest.latitude, dest.longitude),
+        // waypoints: onway(),
+        /* eslint-disable */
+        travelMode: google.maps.TravelMode.DRIVING,
+      })
+      .then((res) => {
+        setDirectionResponse(res);
+      })
+      .catch((err) => console.log(err));
+  }
+
+  console.log("Tanisha", directionResponse);
+  const [currIndex, setcurrindex] = useState(0);
+
+  useEffect(() => {
+    let l1 = location.state.markers[currIndex+1];
+    let l2 = location.state.markers[currIndex];
+    calculateRoute(l1, l2);
+  }, []);
 
   const mapOptions = {
     mapId: "7e437361629e930a",
@@ -91,183 +131,113 @@ function MyComponent() {
     setMap(null);
   }, []);
 
-  //Directions service
-  const [directionResponse, setDirectionResponse] = useState(false);
-  const onway = () => {
-    let t = [];
-    for (var j = 1; j < location.state.markers.length - 1; j++) {
-      let mm = {
-        location: {
-          lat: location.state.markers[j].latitude,
-          lng: location.state.markers[j].longitude,
-        },
-        stopover: true,
-      };
-      t.push(mm);
-    }
-    return t;
-  };
-
-  const ress = [];
-  const damn = [];
-  let ppio = 0;
-
-  async function calculateRoute() {
-    if (!location.state.markers) {
-      return;
-    }
-    /* eslint-disable */
-    const directionService = new google.maps.DirectionsService();
-    let ppoo = [];
-    for (var i = 0; i < location.state.markers.length - 1; i++) {
-      if (directionService) {
-        await directionService
-          .route({
-            origin: new google.maps.LatLng(
-              location.state.markers[i].latitude,
-              location.state.markers[i].longitude
-            ),
-            destination: new google.maps.LatLng(
-              location.state.markers[i + 1].latitude,
-              location.state.markers[i + 1].longitude
-            ),
-            // waypoints:onway(),
-            /* eslint-disable */
-            travelMode: google.maps.TravelMode.TRANSIT,
-          })
-          .then((res) => {
-            console.log(res);
-            ppoo.push(res);
-          })
-          .catch((err) => console.log(err));
-      }
-    }
-    console.log(ppoo);
-    setDirectionResponse(ppoo);
-  }
-  console.log("diex", directionResponse);
-  if (directionResponse) {
-    for (var oo = 0; oo < directionResponse.length; oo++) {
-      damn.push(ppio);
-      ppio = ppio + 1;
-    }
-  }
-  console.log(damn);
+  console.log(currIndex);
   useEffect(() => {
-    calculateRoute();
-  }, []);
-  const [num, setnum] = useState(0);
-  // const getnextid = () => {
-  //   setnum(num + 1);
-  //   // return num;
-  // };
+    let l4 = location.state.markers[currIndex+1];
+    let l5 = location.state.markers[currIndex];
+    calculateRoute(l4, l5);
+  }, [currIndex]);
 
-  // const getprevid = () => {
-  //   setnum(num - 1);
-  //   // return num;
-  // };
-  // const llo = [1,2,3];
-  // console.log("ow",onway())
-  // console.log("damn", damn);
-  // console.log(num)
-  // console.log("direction", directionResponse);
+  console.log(location.state);
   return isLoaded ? (
     <>
-      {directionResponse && (
-        <>
-          <div className="flex justify-center items-center ">
-            <div className="mt-[4%]">
-              {/* {damn.length != 0 ? (
+      {/* {directionResponse && (
+        <> */}
+      <div className="flex justify-center items-center ">
+        <div className="mt-[4%]">
+          {/* {damn.length != 0 ? (
                 <>
                   {damn.map((item22) => {
                     console.log(directionResponse);
                     return (
                       <> */}
-              <GoogleMap
-                mapContainerStyle={containerStyle}
-                zoom={10}
-                options={mapOptions}
-                onLoad={onLoad}
-                onUnmount={onUnmount}
-              >
-                {location.state.markers.map((item) => {
-                  return (
-                    <MarkerF
-                      position={{
-                        lat: item.latitude,
-                        lng: item.longitude,
-                      }}
-                    />
-                  );
-                })}
+          <GoogleMap
+            mapContainerStyle={containerStyle}
+            zoom={10}
+            options={mapOptions}
+            onLoad={onLoad}
+            onUnmount={onUnmount}
+          >
+            {/* {location.state.markers.map((item) => {
+              return (
+                <MarkerF
+                  position={{
+                    lat: item.latitude,
+                    lng: item.longitude,
+                  }}
+                />
+              );
+            })} */}
+            <MarkerF position={{lat:location.state.markers[currIndex].latitude,lng:location.state.markers[currIndex].longitude}}/>
+            <MarkerF position={{lat:location.state.markers[currIndex+1].latitude,lng:location.state.markers[currIndex+1].longitude}}/>
 
-                {directionResponse && (
-                  <DirectionsRenderer directions={directionResponse[num]} />
-                )}
-              </GoogleMap>
-              {/* </>
+            {directionResponse && (
+              <DirectionsRenderer directions={directionResponse} />
+            )}
+          </GoogleMap>
+          {/* </>
                     );
                   })}
                 </>
               ) : (
                 <></>
               )} */}
-            </div>
-          </div>
+        </div>
+      </div>
 
-          <div className="flex justify-center items-center w-[100vw] gap-10">
-            <div
-              className="button"
-              onClick={() => {
-                if (num > 0) setnum(num - 1);
-                else {
-                  toast({
-                    title: "Previous route not available",
-                    status: "warning",
-                    isClosable: true,
-                    position: "bottom",
-                  });
-                }
-              }}
-            >
-              <div class="box-border relative z-30 inline-flex items-center justify-center w-auto px-8 py-3 overflow-hidden font-bold text-white transition-all duration-300 bg-indigo-600 rounded-md cursor-pointer group ring-offset-2 ring-1 ring-indigo-300 ring-offset-indigo-200 hover:ring-offset-indigo-500 ease focus:outline-none">
-                <span class="absolute bottom-0 right-0 w-8 h-20 -mb-8 -mr-5 transition-all duration-300 ease-out transform rotate-45 translate-x-1 bg-white opacity-10 group-hover:translate-x-0"></span>
-                <span class="absolute top-0 left-0 w-20 h-8 -mt-1 -ml-12 transition-all duration-300 ease-out transform -rotate-45 -translate-x-1 bg-white opacity-10 group-hover:translate-x-0"></span>
-                <span class="relative z-20 flex items-center text-sm gap-2">
-                  <AiFillLock />
-                  Previous
-                </span>
-              </div>
-            </div>
-
-            <div
-              className="button"
-              onClick={() => {
-                if (num + 1 < directionResponse.length) setnum(num + 1);
-                else {
-                  toast({
-                    title: "Next route not available",
-                    status: "warning",
-                    isClosable: true,
-                    position: "bottom",
-                  });
-                }
-              }}
-            >
-              <div class="box-border relative z-30 inline-flex items-center justify-center w-auto px-8 py-3 overflow-hidden font-bold text-white transition-all duration-300 bg-indigo-600 rounded-md cursor-pointer group ring-offset-2 ring-1 ring-indigo-300 ring-offset-indigo-200 hover:ring-offset-indigo-500 ease focus:outline-none">
-                <span class="absolute bottom-0 right-0 w-8 h-20 -mb-8 -mr-5 transition-all duration-300 ease-out transform rotate-45 translate-x-1 bg-white opacity-10 group-hover:translate-x-0"></span>
-                <span class="absolute top-0 left-0 w-20 h-8 -mt-1 -ml-12 transition-all duration-300 ease-out transform -rotate-45 -translate-x-1 bg-white opacity-10 group-hover:translate-x-0"></span>
-                <span class="relative z-20 flex items-center text-sm gap-2">
-                  <AiFillLock />
-                  Next
-                </span>
-              </div>
-            </div>
+      <div className="flex justify-center items-center w-[100vw] gap-10">
+        <div
+          className="button"
+          onClick={() => {
+            if (currIndex > 0) setcurrindex(currIndex - 1);
+            else {
+              toast({
+                title: "Previous route not available",
+                status: "warning",
+                isClosable: true,
+                position: "bottom",
+              });
+            }
+          }}
+        >
+          <div class="box-border relative z-30 inline-flex items-center justify-center w-auto px-8 py-3 overflow-hidden font-bold text-white transition-all duration-300 bg-indigo-600 rounded-md cursor-pointer group ring-offset-2 ring-1 ring-indigo-300 ring-offset-indigo-200 hover:ring-offset-indigo-500 ease focus:outline-none">
+            <span class="absolute bottom-0 right-0 w-8 h-20 -mb-8 -mr-5 transition-all duration-300 ease-out transform rotate-45 translate-x-1 bg-white opacity-10 group-hover:translate-x-0"></span>
+            <span class="absolute top-0 left-0 w-20 h-8 -mt-1 -ml-12 transition-all duration-300 ease-out transform -rotate-45 -translate-x-1 bg-white opacity-10 group-hover:translate-x-0"></span>
+            <span class="relative z-20 flex items-center text-sm gap-2">
+              <AiFillLock />
+              Previous
+            </span>
           </div>
-        </>
-      )}
+        </div>
+
+        <div
+          className="button"
+          onClick={() => {
+            if (currIndex + 1 < location.state.markers.length - 1) {
+              setcurrindex(currIndex + 1);
+            } else {
+              toast({
+                title: "Next route not available",
+                status: "warning",
+                isClosable: true,
+                position: "bottom",
+              });
+            }
+          }}
+        >
+          <div class="box-border relative z-30 inline-flex items-center justify-center w-auto px-8 py-3 overflow-hidden font-bold text-white transition-all duration-300 bg-indigo-600 rounded-md cursor-pointer group ring-offset-2 ring-1 ring-indigo-300 ring-offset-indigo-200 hover:ring-offset-indigo-500 ease focus:outline-none">
+            <span class="absolute bottom-0 right-0 w-8 h-20 -mb-8 -mr-5 transition-all duration-300 ease-out transform rotate-45 translate-x-1 bg-white opacity-10 group-hover:translate-x-0"></span>
+            <span class="absolute top-0 left-0 w-20 h-8 -mt-1 -ml-12 transition-all duration-300 ease-out transform -rotate-45 -translate-x-1 bg-white opacity-10 group-hover:translate-x-0"></span>
+            <span class="relative z-20 flex items-center text-sm gap-2">
+              <AiFillLock />
+              Next
+            </span>
+          </div>
+        </div>
+      </div>
     </>
   ) : (
+    //     )}
     <></>
   );
 }
