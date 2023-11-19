84bd215f8703db25d94aed7dfcc8b689aa68f152
Abhinav Singh <kittusinghranchi@gmail.com>
2023-11-19 10:36:38 +0530

    minor changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,16 +5,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./Pages/Homepage/Homepage";
 import Maps from "./Pages/Map/RouteMap";
 import LoginPage from "./Pages/LoginPage/LoginPage";
+import AddUser from "./Pages/AddUser/AddUser";
 
 function App() {
   return (
     <>
-      <Navbar />
+
       <BrowserRouter>
+      <Navbar/>
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/routeDetails" element={<Maps />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/addUser" element={<AddUser />} />
         </Routes>
       </BrowserRouter>
     </>
